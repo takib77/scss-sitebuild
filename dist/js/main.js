199dfd6652e20a148d845425c1370a34b755bbb4
Leo <takib77@gmail.com>
2021-01-17 17:39:46 +0100

    Menu added

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,28 +1,28 @@
 'use strict';
 
 const headerStyle = () => {
-    const header = document.querySelector('.sticky-top');
-    if (window.scrollY < 10) header.className = 'sticky-top justify-content-center p-0 bg-transparent';
-    else header.className = 'sticky-top justify-content-center p-0 bg-white';
+    const header = document.querySelector('.nav');
+    if (window.scrollY < 10) header.className = 'nav';
+    else header.className = 'nav bg-white';
 };
 
 const brandStyle = () => {
-    const navbarBrand = document.querySelector('.navbar-brand')
-    if (window.scrollY < 10) navbarBrand.className = 'navbar-brand text-warning';
-    else navbarBrand.className = 'navbar-brand text-dark';
+    const navbarBrand = document.querySelector('.nav__brandlink')
+    if (window.scrollY < 10) navbarBrand.className = 'nav__brandlink text-warning';
+    else navbarBrand.className = 'nav__brandlink text-black';
 };
 
 const linkStyle = () => {
-    const navLink = document.querySelectorAll('.nav-link')
+    const navLink = document.querySelectorAll('.nav__link')
     if (window.scrollY < 10) {
-        navLink[0].className = 'nav-link text-white';
-        navLink[1].className = 'nav-link text-white';
-        navLink[2].className = 'nav-link text-white';
+        navLink[0].className = 'nav__link text-white';
+        navLink[1].className = 'nav__link text-white';
+        navLink[2].className = 'nav__link text-white';
     }
     else {
-        navLink[0].className = 'nav-link text-dark';
-        navLink[1].className = 'nav-link text-dark';
-        navLink[2].className = 'nav-link text-dark';
+        navLink[0].className = 'nav__link text-black';
+        navLink[1].className = 'nav__link text-black';
+        navLink[2].className = 'nav__link text-black';
     }
 };
 
